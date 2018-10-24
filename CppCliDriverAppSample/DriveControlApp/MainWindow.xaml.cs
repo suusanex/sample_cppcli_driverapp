4f77783eaa429a2598b507d8100d90d94076a2ee
ksudoh <ksudoh@sciencepark.co.jp>
2018-10-24 15:04:39 +0900

    アプリとDLLを新規作成し、動作確認完了。

---
+++ CppCliDriverAppSample/DriveControlApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CppCliDll;
+
+namespace DriveControlApp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick_DeviceIoControl(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var inst = new Volume();
+                inst.VolumeCommand(new InData {Data1 = 3});
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"エラー：{exception.Message}");
+            }
+        }
+    }
+}
